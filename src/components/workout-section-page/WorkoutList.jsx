5400d75e5fcec92ecd567adc0e8ac9a4618c1797
Vasilios Kambouras <Vasilioskw@gmail.com>
2021-07-10 10:35:36 -0400

    card updated to allow selectionn of weight and reps

--- src/components/workout-section-page/WorkoutList.jsx
+++ src/components/workout-section-page/WorkoutList.jsx
@@ -1,6 +1,6 @@
 import React from "react"; 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Card, Button } from 'react-bootstrap';
+import { Card, Button, Form } from 'react-bootstrap';
 import './Workoutpage.CSS';
 
 function WorkoutList() {
@@ -11,10 +11,28 @@ function WorkoutList() {
 <Card style={{width: '32rem', textAlign: 'center'}}>
   <Card.Header> Exercise Name </Card.Header>
   <Card.Body>
-    <Card.Title>weight 125lbs</Card.Title>
-    <Card.Text> (Number of reps) </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
+
+    <Card.Title>
+    <Form>
+  <Form.Group controlId="exampleForm.ControlSelect1">
+    <Form.Label>How much weight do you want?</Form.Label>
+    <Form.Control as="select">
+      <option>125 lbs</option>
+    </Form.Control>
+  </Form.Group>
+<br/>
+  <Form.Group controlId="exampleForm.ControlSelect1">
+    <Form.Label>How many reps do you want?</Form.Label>
+    <Form.Control as="select">
+      <option>5</option>
+    </Form.Control>
+  </Form.Group>
+</Form>   
+</Card.Title> 
+<br/>
+<Button variant="primary">Go somewhere</Button>
   </Card.Body>
+  <br/>
 </Card>
       </div>
       </center> 
