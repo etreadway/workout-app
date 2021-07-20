bb04a1c461234a59c32bdceb8c52f0ba869a0989
Vasilios Kambouras <Vasilioskw@gmail.com>
2021-07-20 19:36:46 -0400

    new branch

--- src/components/Workout.jsx
+++ src/components/Workout.jsx
@@ -44,14 +44,14 @@ function Workout(props) {
 }
 
 // <div className="workout" onClick={handleClick}>
-{/* <h3>Title</h3>
-<p>Aenean at tempor ligula. Aliquam justo ligula, viverra sit amet vestibulum id, commodo quis nunc. Proin in velit vel ex dignissim pretium at pellentesque dolor. Mauris porta metus vitae finibus tincidunt. Curabitur eleifend diam sit amet ex rutrum elementum nec in turpis. Quisque fringilla sem ullamcorper feugiat posuere. Nulla mollis odio a augue rhoncus, ac porta nisl cursus.</p>
-<ul className={`workout-items ${isClicked ? "expand" : ""}`}>
-    <li>Item 1</li>
-    <li>Item 2</li>
-    <li>Item 3</li>
-//     {`workout-items ${isClicked ? "expand" : ""}
-// </ul>
-// </div> */}
+// {/* <h3>Title</h3>
+// <p>Aenean at tempor ligula. Aliquam justo ligula, viverra sit amet vestibulum id, commodo quis nunc. Proin in velit vel ex dignissim pretium at pellentesque dolor. Mauris porta metus vitae finibus tincidunt. Curabitur eleifend diam sit amet ex rutrum elementum nec in turpis. Quisque fringilla sem ullamcorper feugiat posuere. Nulla mollis odio a augue rhoncus, ac porta nisl cursus.</p>
+// <ul className={`workout-items ${isClicked ? "expand" : ""}`}>
+//     <li>Item 1</li>
+//     <li>Item 2</li>
+//     <li>Item 3</li>
+// //     {`workout-items ${isClicked ? "expand" : ""}
+// // </ul>
+// // </div> */}
 
 export default Workout;
